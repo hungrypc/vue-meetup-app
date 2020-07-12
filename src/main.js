53a25f66c35e1f58ac7a39e339d570d4028ddc0f
hpc <philipchanhk626@gmail.com>
2020-07-12 14:08:50 -0400

    MeetupItem, v-for meetups, formatDate filter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import moment from 'moment'
 import App from './App.vue'
 import AppDropdown from './components/shared/AppDropdown'
 import AppHero from './components/shared/AppHero'
@@ -7,11 +8,17 @@ Vue.config.productionTip = false
 
 Vue.component('AppHero', AppHero)
 Vue.component('AppDropdown', AppDropdown)
+
 Vue.filter('capitalize', function (value) {
   if (value && typeof value === 'string') {
     return value.charAt(0).toUpperCase() + value.slice(1)
   }
   return ''
+})
+
+Vue.filter('formatDate', function(value, formatType='LL') {
+  if (!value) return ''
+  return moment(value).format(formatType)
 })
 
 new Vue({
