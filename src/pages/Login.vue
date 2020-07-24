c448cf4d7a98cfee9a14ffcc726a147719212446
hpc <philipchanhk626@gmail.com>
2020-07-24 09:36:55 -0400

    login form validate on blur, submit button disabled unless form is valid

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -7,18 +7,20 @@
           <p class="subtitle has-text-grey">Please login to proceed.</p>
           <div class="box">
             <figure class="avatar">
-                <img src="https://placehold.it/128x128">
+              <img src="https://placehold.it/128x128" />
             </figure>
             <form>
               <div class="field">
                 <div class="control">
-                  <input class="input is-large"
-                         type="email"
-                         placeholder="Your Email"
-                         autofocus=""
-                         autocomplete="email"
-                         v-model="form.email"
-                  >
+                  <input
+                    class="input is-large"
+                    @blur="$v.form.email.$touch()"
+                    type="email"
+                    placeholder="Your Email"
+                    autofocus
+                    autocomplete="email"
+                    v-model="form.email"
+                  />
                   <div v-if="$v.form.email.$error" class="form-error">
                     <span v-if="!$v.form.email.required" class="help is-danger">Email is required</span>
                     <span v-if="!$v.form.email.email" class="help is-danger">Invalid email</span>
@@ -27,23 +29,32 @@
               </div>
               <div class="field">
                 <div class="control">
-                  <input class="input is-large"
-                         type="password"
-                         placeholder="Your Password"
-                         autocomplete="current-password"
-                         v-model="form.password"
-                  >
+                  <input
+                    class="input is-large"
+                    @blur="$v.form.password.$touch()"
+                    type="password"
+                    placeholder="Your Password"
+                    autocomplete="current-password"
+                    v-model="form.password"
+                  />
                   <div v-if="$v.form.password.$error" class="form-error">
-                    <span class="help is-danger">Invalid password</span>
+                    <span
+                      v-if="!$v.form.password.required"
+                      class="help is-danger"
+                    >Password is required</span>
                   </div>
                 </div>
               </div>
-              <button @click.prevent="login" class="button is-block is-info is-large is-fullwidth">Login</button>
+              <button
+                @click.prevent="login"
+                class="button is-block is-info is-large is-fullwidth"
+                :disabled="isFormInvalid"
+              >Login</button>
             </form>
           </div>
           <p class="has-text-grey">
             <a>Sign In With Google</a> &nbsp;·&nbsp;
-            <router-link :to="{name: 'Register'}">Sign Up</router-link> &nbsp;·&nbsp;
+            <router-link :to="{name: 'Register'}">Sign Up</router-link>&nbsp;·&nbsp;
             <a href="../">Need Help?</a>
           </p>
         </div>
@@ -53,66 +64,73 @@
 </template>
 
 <script>
-  import { required, email } from 'vuelidate/lib/validators'
+import { required, email } from "vuelidate/lib/validators";
 
-  export default {
-    data() {
-      return {
-        form: {
-          email: null,
-          password: null
-        }
-      }
-    },
-    validations: {
+export default {
+  data() {
+    return {
       form: {
-        email: {
-          required,
-          email
-        },
-        password: {
-          required
-        }
+        email: null,
+        password: null
       }
-    },
-    methods: {
-      login() {
-        this.$v.form.$touch()
-        this.$store.dispatch('auth/userLogin', this.form)
+    };
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required
       }
     }
+  },
+  computed: {
+    isFormInvalid() {
+      return this.$v.form.$invalid
+    }
+  },
+  methods: {
+    login() {
+      this.$v.form.$touch();
+      this.$store.dispatch("auth/userLogin", this.form);
+    }
   }
+};
 </script>
 
 <style scoped>
-  .hero.is-success {
-    background: #F2F6FA;
-  }
-  .hero .nav, .hero.is-success .nav {
-    -webkit-box-shadow: none;
-    box-shadow: none;
-  }
-  .box {
-    margin-top: 5rem;
-  }
-  .avatar {
-    margin-top: -70px;
-    padding-bottom: 20px;
-  }
-  .avatar img {
-    padding: 5px;
-    background: #fff;
-    border-radius: 50%;
-    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-  }
-  input {
-    font-weight: 300;
-  }
-  p {
-    font-weight: 700;
-  }
-  p.subtitle {
-    padding-top: 1rem;
-  }
+.hero.is-success {
+  background: #f2f6fa;
+}
+.hero .nav,
+.hero.is-success .nav {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.box {
+  margin-top: 5rem;
+}
+.avatar {
+  margin-top: -70px;
+  padding-bottom: 20px;
+}
+.avatar img {
+  padding: 5px;
+  background: #fff;
+  border-radius: 50%;
+  -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
+    0 0 0 1px rgba(10, 10, 10, 0.1);
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+}
+input {
+  font-weight: 300;
+}
+p {
+  font-weight: 700;
+}
+p.subtitle {
+  padding-top: 1rem;
+}
 </style>
